dc9f546a3d309b62952a3a8ccb4bf4df5c904669
Marcel Ludwig <london_marcel@hotmail.com>
2024-06-09 14:57:46 -0300

    Printing possible moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -25,12 +25,19 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	//movimentos possiveis de uma peça
+	public boolean[][] possibleMoves(ChessPosition sourcePosition){
+		Position position = sourcePosition.toPosition();
+		validateSourcePosition(position);
+		return board.piece(position).possibleMoves();
+	}
+	
 	//mover a peça
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
 		Position source = sourcePosition.toPosition();
 		Position target =  targetPosition.toPosition();
 		validateSourcePosition(source);
-		validadeTargetPosition(source, target);
+		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece)capturedPiece;
 	}
@@ -53,11 +60,12 @@ public class ChessMatch {
 		}
 	}
 	
-	private void validadeTargetPosition(Position source, Position target) {
+	private void validateTargetPosition(Position source, Position target) {
 		if(!board.piece(source).possibleMove(target)) {
 			throw new ChessException("The chosen piece can't move to target position");
 		}
 	}
+	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
